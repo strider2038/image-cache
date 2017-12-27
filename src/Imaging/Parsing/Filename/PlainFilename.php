4d38de98ef6977ce1946813b9e0d90e2da9f3bea
Igor Lazarev <strider2038@yandex.ru>
2017-12-27 22:54:47 +0300

    PlainFilename validation rules added

--- src/Imaging/Parsing/Filename/PlainFilename.php
+++ src/Imaging/Parsing/Filename/PlainFilename.php
@@ -10,12 +10,37 @@
 
 namespace Strider2038\ImgCache\Imaging\Parsing\Filename;
 
+use Strider2038\ImgCache\Core\ModelInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @author Igor Lazarev <strider2038@rambler.ru>
  */
-class PlainFilename
+class PlainFilename implements ModelInterface
 {
-    /** @var string */
+    /**
+     * @Assert\NotBlank()
+     * @Assert\NotIdenticalTo("/")
+     * @Assert\Regex(
+     *     pattern="/^[A-Za-z0-9_\-\.\/]+$/",
+     *     message="Filename must contain only latin symbols, digits, snakes '_', dots '.' and slashes '/'"
+     * )
+     * @Assert\Regex(
+     *     pattern="/\/{2,}|\.{2,}/",
+     *     match=false,
+     *     message="Filename cannot contain duplicating slashes or dots"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^.*\..*\/(.*)/",
+     *     match=false,
+     *     message="Dots are not allowed in directory names"
+     * )
+     * @Assert\Regex(
+     *     pattern="/.*\.(jpg|jpeg|png)$/",
+     *     message="Only 'jpg', 'jpeg' and 'png' lowercase extensions allowed"
+     * )
+     * @var string
+     */
     private $value;
 
     public function __construct(string $value)
--- tests/Unit/Imaging/Parsing/Filename/PlainFilenameTest.php
+++ tests/Unit/Imaging/Parsing/Filename/PlainFilenameTest.php
@@ -12,10 +12,20 @@ namespace Strider2038\ImgCache\Tests\Unit\Imaging\Parsing\Filename;
 
 use PHPUnit\Framework\TestCase;
 use Strider2038\ImgCache\Imaging\Parsing\Filename\PlainFilename;
+use Strider2038\ImgCache\Imaging\Validation\ModelValidator;
+use Strider2038\ImgCache\Imaging\Validation\ModelValidatorInterface;
 
 class PlainFilenameTest extends TestCase
 {
     private const VALUE = 'a.jpg';
+
+    /** @var ModelValidatorInterface */
+    private $validator;
+
+    protected function setUp(): void
+    {
+        $this->validator = new ModelValidator();
+    }
 
     /** @test */
     public function construct_givenValue_valueSet(): void
@@ -23,5 +33,51 @@ class PlainFilenameTest extends TestCase
         $key = new PlainFilename(self::VALUE);
 
         $this->assertEquals(self::VALUE, $key->getValue());
+    }
+
+    /**
+     * @test
+     * @dataProvider valueProvider
+     * @param string $value
+     * @param int $violationsCount
+     */
+    public function validate_givenFilename_violationsReturned(string $value, int $violationsCount): void
+    {
+        $filename = new PlainFilename($value);
+
+        $violations = $this->validator->validateModel($filename);
+
+        $this->assertCount($violationsCount, $violations);
+    }
+
+    public function valueProvider(): array
+    {
+        return [
+            /*  0 */ ['', 1],
+            /*  1 */ ['  ', 2],
+            /*  2 */ ['/', 2],
+            /*  3 */ [' /', 2],
+            /*  4 */ ['//', 2],
+            /*  5 */ ['file .jpg', 1],
+            /*  6 */ ['кириллица.jpg', 1],
+            /*  7 */ ['/path/\1', 2],
+            /*  8 */ ['file.err', 1],
+            /*  9 */ ['../file.jpg', 2],
+            /* 10 */ ['.../i.jpg', 2],
+            /* 11 */ ['f../i.jpg', 2],
+            /* 12 */ ['/../file.jpg', 2],
+            /* 13 */ ['dir.name/f.jpeg', 1],
+            /* 14 */ ['/.dir/f.jpg', 1],
+            /* 15 */ ['dir/f.jpg', 0],
+            /* 16 */ ['dir//f.jpg', 1],
+            /* 17 */ ['./f.jpeg', 1],
+            /* 18 */ ['/./f.jpeg', 1],
+            /* 19 */ ['Img.jpg', 0],
+            /* 20 */ ['/path/image.jpg', 0],
+            /* 21 */ ['/path/image..jpg', 1],
+            /* 22 */ ['/path/image_sz80x100.jpg', 0],
+            /* 23 */ ['//path//image_sz80x100.jpg', 1],
+            /* 24 */ ['_root/i-2_q95.jpeg', 0],
+        ];
     }
 }
