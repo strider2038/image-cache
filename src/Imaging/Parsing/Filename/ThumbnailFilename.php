41ca35bb4809f5c5def31aa46a88ee725f1bf50a
strider <strider2038@yandex.ru>
2018-01-04 21:01:13 +0300

    ThumbnailFilename supports validation rules

--- src/Imaging/Parsing/Filename/ThumbnailFilename.php
+++ src/Imaging/Parsing/Filename/ThumbnailFilename.php
@@ -10,28 +10,68 @@
 
 namespace Strider2038\ImgCache\Imaging\Parsing\Filename;
 
-use Strider2038\ImgCache\Imaging\Parsing\Filename\PlainFilename;
-use Strider2038\ImgCache\Imaging\Processing\ProcessingConfiguration;
+use Strider2038\ImgCache\Core\EntityInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @author Igor Lazarev <strider2038@rambler.ru>
  */
-class ThumbnailFilename extends PlainFilename
+class ThumbnailFilename implements EntityInterface
 {
-    /** @var ProcessingConfiguration */
-    private $processingConfiguration;
+    /**
+     * @Assert\NotBlank()
+     * @Assert\NotIdenticalTo("/")
+     * @Assert\Regex(
+     *     pattern="/^[A-Za-z0-9_\-\.\/]+$/",
+     *     message="Filename must contain only latin symbols, digits, snakes '_', dots '.' and slashes '/'"
+     * )
+     * @Assert\Regex(
+     *     pattern="/\/{2,}|\.{2,}/",
+     *     match=false,
+     *     message="Filename cannot contain duplicating slashes or dots"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^.*\..*\/(.*)/",
+     *     match=false,
+     *     message="Dots are not allowed in directory names"
+     * )
+     * @Assert\Regex(
+     *     pattern="/.*\.(jpg|jpeg|png)$/",
+     *     message="Only 'jpg', 'jpeg' and 'png' lowercase extensions allowed"
+     * )
+     * @Assert\Regex(
+     *     pattern="/.*_\.[a-z]{3,4}$/",
+     *     match=false,
+     *     message="Empty processing configuration is not allowed"
+     * )
+     * @var string
+     */
+    private $value;
 
     /** @var string */
     private $mask;
 
+    /** @var string */
+    private $processingConfiguration;
+
     public function __construct(
         string $value,
         string $mask,
-        ProcessingConfiguration $processingConfiguration
+        string $processingConfiguration
     ) {
-        parent::__construct($value);
+        $this->value = $value;
         $this->mask = $mask;
         $this->processingConfiguration = $processingConfiguration;
+    }
+
+    public function getId(): string
+    {
+        return 'thumbnail filename';
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
     }
 
     public function getMask(): string
@@ -39,13 +79,13 @@ class ThumbnailFilename extends PlainFilename
         return $this->mask;
     }
 
-    public function getProcessingConfiguration(): ProcessingConfiguration
+    public function getProcessingConfiguration(): string
     {
         return $this->processingConfiguration;
     }
 
     public function hasProcessingConfiguration(): bool
     {
-        return !$this->processingConfiguration->isDefault();
+        return $this->processingConfiguration !== '';
     }
 }
--- tests/Unit/Imaging/Parsing/Filename/ThumbnailFilenameTest.php
+++ tests/Unit/Imaging/Parsing/Filename/ThumbnailFilenameTest.php
@@ -12,56 +12,122 @@ namespace Strider2038\ImgCache\Tests\Unit\Imaging\Parsing\Filename;
 
 use PHPUnit\Framework\TestCase;
 use Strider2038\ImgCache\Imaging\Parsing\Filename\ThumbnailFilename;
-use Strider2038\ImgCache\Imaging\Processing\ProcessingConfiguration;
-use Strider2038\ImgCache\Tests\Support\Phake\ProviderTrait;
+use Strider2038\ImgCache\Utility\EntityValidator;
+use Strider2038\ImgCache\Utility\EntityValidatorInterface;
+use Strider2038\ImgCache\Utility\ViolationFormatter;
 
 class ThumbnailFilenameTest extends TestCase
 {
-    use ProviderTrait;
-
     private const VALUE = 'a.jpg';
     private const MASK = 'thumbnail_mask';
+    private const PROCESSING_CONFIGURATION = 'processing_configuration';
+    private const THUMBNAIL_FILENAME_ID = 'thumbnail filename';
 
-    /** @var ProcessingConfiguration */
-    private $processingConfiguration;
+    /** @var EntityValidatorInterface */
+    private $validator;
 
     protected function setUp(): void
     {
-        $this->processingConfiguration = \Phake::mock(ProcessingConfiguration::class);
+        $this->validator = new EntityValidator(new ViolationFormatter());
     }
 
     /** @test */
     public function construct_givenProperties_propertiesAreSet(): void
     {
-        $key = $this->createThumbnailKey();
+        $filename = $this->createThumbnailFilename();
 
-        $this->assertEquals(self::VALUE, $key->getValue());
-        $this->assertEquals(self::MASK, $key->getMask());
-        $this->assertSame($this->processingConfiguration, $key->getProcessingConfiguration());
+        $this->assertEquals(self::VALUE, $filename->getValue());
+        $this->assertEquals(self::MASK, $filename->getMask());
+        $this->assertEquals(self::PROCESSING_CONFIGURATION, $filename->getProcessingConfiguration());
+    }
+
+    /** @test */
+    public function getId_emptyParameters_idReturned(): void
+    {
+        $filename = $this->createThumbnailFilename();
+
+        $id = $filename->getId();
+
+        $this->assertEquals(self::THUMBNAIL_FILENAME_ID, $id);
     }
 
     /**
      * @test
-     * @param bool $isDefault
-     * @dataProvider boolValuesProvider
+     * @param string $processingConfiguration
+     * @param bool $expectedHasProcessingConfiguration
+     * @dataProvider processingConfigurationProvider
      */
-    public function hasProcessingConfiguration_givenProcessingConfiguration_boolIsReturned(
-        bool $isDefault
+    public function hasProcessingConfiguration_givenProcessingConfiguration_expectedValueReturned(
+        string $processingConfiguration,
+        bool $expectedHasProcessingConfiguration
     ): void {
-        $key = $this->createThumbnailKey();
-        \Phake::when($this->processingConfiguration)->isDefault()->thenReturn($isDefault);
+        $filename = $this->createThumbnailFilename(self::VALUE, self::MASK, $processingConfiguration);
 
-        $result = $key->hasProcessingConfiguration();
+        $hasProcessingConfiguration = $filename->hasProcessingConfiguration();
 
-        $this->assertEquals(!$isDefault, $result);
+        $this->assertEquals($expectedHasProcessingConfiguration, $hasProcessingConfiguration);
     }
 
-    private function createThumbnailKey(): ThumbnailFilename
+    public function processingConfigurationProvider(): array
     {
-        return new ThumbnailFilename(
-            self::VALUE,
-            self::MASK,
-            $this->processingConfiguration
-        );
+        return [
+            ['', false],
+            ['_', true]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider valueProvider
+     * @param string $value
+     * @param int $violationsCount
+     */
+    public function validate_givenFilename_violationsReturned(string $value, int $violationsCount): void
+    {
+        $filename = $this->createThumbnailFilename($value);
+
+        $violations = $this->validator->validate($filename);
+
+        $this->assertCount($violationsCount, $violations);
+    }
+
+    public function valueProvider(): array
+    {
+        return [
+            /*  0 */ ['', 1],
+            /*  1 */ ['  ', 2],
+            /*  2 */ ['/', 2],
+            /*  3 */ [' /', 2],
+            /*  4 */ ['//', 2],
+            /*  5 */ ['file .jpg', 1],
+            /*  6 */ ['кириллица.jpg', 1],
+            /*  7 */ ['/path/\1', 2],
+            /*  8 */ ['file.err', 1],
+            /*  9 */ ['../file.jpg', 2],
+            /* 10 */ ['.../i.jpg', 2],
+            /* 11 */ ['f../i.jpg', 2],
+            /* 12 */ ['/../file.jpg', 2],
+            /* 13 */ ['dir.name/f.jpeg', 1],
+            /* 14 */ ['/.dir/f.jpg', 1],
+            /* 15 */ ['dir/f.jpg', 0],
+            /* 16 */ ['dir//f.jpg', 1],
+            /* 17 */ ['./f.jpeg', 1],
+            /* 18 */ ['/./f.jpeg', 1],
+            /* 19 */ ['Img.jpg', 0],
+            /* 20 */ ['/path/image.jpg', 0],
+            /* 21 */ ['/path/image..jpg', 1],
+            /* 22 */ ['/path/image_sz80x100.jpg', 0],
+            /* 23 */ ['//path//image_sz80x100.jpg', 1],
+            /* 24 */ ['_root/i-2_q95.jpeg', 0],
+            /* 25 */ ['i_.jpeg', 1],
+        ];
+    }
+
+    private function createThumbnailFilename(
+        $value = self::VALUE,
+        $mask = self::MASK,
+        $processingConfiguration = self::PROCESSING_CONFIGURATION
+    ): ThumbnailFilename {
+        return new ThumbnailFilename($value, $mask, $processingConfiguration);
     }
 }
