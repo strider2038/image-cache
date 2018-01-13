5345868c2162398278f53b0cd6642b6c78ab2c4d
Igor Lazarev <strider2038@yandex.ru>
2018-01-13 12:59:10 +0300

    ShiftTransformation introduced

---
+++ src/Imaging/Processing/Transforming/ShiftTransformation.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of ImgCache.
+ *
+ * (c) Igor Lazarev <strider2038@rambler.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strider2038\ImgCache\Imaging\Processing\Transforming;
+
+use Strider2038\ImgCache\Imaging\Processing\ImageTransformerInterface;
+use Strider2038\ImgCache\Imaging\Processing\PointInterface;
+
+/**
+ * @author Igor Lazarev <strider2038@rambler.ru>
+ */
+class ShiftTransformation implements TransformationInterface
+{
+    /** @var PointInterface */
+    private $parameters;
+
+    public function __construct(PointInterface $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    public function getParameters(): PointInterface
+    {
+        return $this->parameters;
+    }
+
+    public function apply(ImageTransformerInterface $transformer): void
+    {
+        $transformer->shift($this->parameters);
+    }
+}
