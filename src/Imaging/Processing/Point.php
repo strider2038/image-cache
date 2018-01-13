fb38cd6007a3914bb8722802d3c85c8ea68fcbb0
Igor Lazarev <strider2038@yandex.ru>
2018-01-13 11:53:35 +0300

    Point data object added

---
+++ src/Imaging/Processing/Point.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * This file is part of ImgCache.
+ *
+ * (c) Igor Lazarev <strider2038@rambler.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strider2038\ImgCache\Imaging\Processing;
+
+/**
+ * @author Igor Lazarev <strider2038@rambler.ru>
+ */
+class Point
+{
+    /** @var int */
+    private $x;
+
+    /** @var int */
+    private $y;
+
+    public function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+}
