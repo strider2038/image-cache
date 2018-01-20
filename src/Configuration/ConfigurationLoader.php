1978ee145c4f8fd749b178ad8a0337555b195821
Igor Lazarev <strider2038@yandex.ru>
2018-01-20 13:51:54 +0300

    ConfigurationLoader service introduced

---
+++ src/Configuration/ConfigurationLoader.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file is part of ImgCache.
+ *
+ * (c) Igor Lazarev <strider2038@rambler.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strider2038\ImgCache\Configuration;
+
+use Strider2038\ImgCache\Utility\ConfigurationFileParserInterface;
+use Symfony\Component\Config\Definition\Processor;
+
+/**
+ * @author Igor Lazarev <strider2038@rambler.ru>
+ */
+class ConfigurationLoader implements ConfigurationLoaderInterface
+{
+    private const CONFIGURATION_FILENAME = 'config/parameters.yml';
+
+    /** @var ConfigurationFileParserInterface */
+    private $configurationFileParser;
+
+    /** @var Processor */
+    private $configurationProcessor;
+
+    /** @var ConfigurationFactoryInterface */
+    private $configurationFactory;
+
+    public function __construct(
+        ConfigurationFileParserInterface $configurationFileParser,
+        Processor $configurationProcessor,
+        ConfigurationFactoryInterface $configurationFactory
+    ) {
+        $this->configurationFileParser = $configurationFileParser;
+        $this->configurationProcessor = $configurationProcessor;
+        $this->configurationFactory = $configurationFactory;
+    }
+
+    public function loadConfiguration(): Configuration
+    {
+        $configurationArray = $this->configurationFileParser->parseConfigurationFile(self::CONFIGURATION_FILENAME);
+
+        $applicationConfiguration = new ApplicationConfiguration();
+        $processedConfiguration = $this->configurationProcessor->processConfiguration(
+            $applicationConfiguration,
+            $configurationArray
+        );
+
+        return $this->configurationFactory->createConfiguration($processedConfiguration);
+    }
+}
---
+++ tests/Unit/Configuration/ConfigurationLoaderTest.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ * This file is part of ImgCache.
+ *
+ * (c) Igor Lazarev <strider2038@rambler.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strider2038\ImgCache\Tests\Unit\Configuration;
+
+use PHPUnit\Framework\TestCase;
+use Strider2038\ImgCache\Configuration\ApplicationConfiguration;
+use Strider2038\ImgCache\Configuration\Configuration;
+use Strider2038\ImgCache\Configuration\ConfigurationFactoryInterface;
+use Strider2038\ImgCache\Configuration\ConfigurationLoader;
+use Strider2038\ImgCache\Utility\ConfigurationFileParserInterface;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationLoaderTest extends TestCase
+{
+    private const CONFIGURATION_FILENAME = 'config/parameters.yml';
+
+    /** @var ConfigurationFileParserInterface */
+    private $configurationFileParser;
+
+    /** @var Processor */
+    private $configurationProcessor;
+
+    /** @var ConfigurationFactoryInterface */
+    private $configurationFactory;
+
+    protected function setUp(): void
+    {
+        $this->configurationFileParser = \Phake::mock(ConfigurationFileParserInterface::class);
+        $this->configurationProcessor = \Phake::mock(Processor::class);
+        $this->configurationFactory = \Phake::mock(ConfigurationFactoryInterface::class);
+    }
+
+    /** @test */
+    public function loadConfiguration_noParameters_configurationLoadedFromFileAndProcessedAndConfigurationClassCreatedAndReturned(): void
+    {
+        $loader = new ConfigurationLoader(
+            $this->configurationFileParser,
+            $this->configurationProcessor,
+            $this->configurationFactory
+        );
+        $configurationArray = $this->givenConfigurationFileParser_parseConfigurationFile_returnsConfigurationArray();
+        $processedConfiguration = $this->givenConfigurationProcessor_processConfiguration_returnsProcessedConfiguration();
+        $expectedConfiguration = $this->givenConfigurationFactory_createConfiguration_returnsConfiguration();
+
+        $configuration = $loader->loadConfiguration();
+
+        $this->assertConfigurationFileParser_parseConfigurationFile_isCalledOnceWithFilename(self::CONFIGURATION_FILENAME);
+        $this->assertConfigurationProcessor_processConfiguration_isCalledOnceWithInstanceOfApplicationConfigurationAndConfigurationArray(
+            ApplicationConfiguration::class,
+            $configurationArray
+        );
+        $this->assertConfigurationFactory_createConfiguration_isCalledOnceWithArray($processedConfiguration);
+        $this->assertSame($expectedConfiguration, $configuration);
+    }
+
+    private function givenConfigurationFileParser_parseConfigurationFile_returnsConfigurationArray(): array
+    {
+        $configurationArray = ['configuration_array'];
+        \Phake::when($this->configurationFileParser)
+            ->parseConfigurationFile(\Phake::anyParameters())
+            ->thenReturn($configurationArray);
+
+        return $configurationArray;
+    }
+
+    private function givenConfigurationProcessor_processConfiguration_returnsProcessedConfiguration(): array
+    {
+        $processedConfiguration = ['processed_configuration'];
+        \Phake::when($this->configurationProcessor)
+            ->processConfiguration(\Phake::anyParameters())
+            ->thenReturn($processedConfiguration);
+
+        return $processedConfiguration;
+    }
+
+    private function assertConfigurationFileParser_parseConfigurationFile_isCalledOnceWithFilename(string $filename): void
+    {
+        \Phake::verify($this->configurationFileParser, \Phake::times(1))
+            ->parseConfigurationFile($filename);
+    }
+
+    private function assertConfigurationProcessor_processConfiguration_isCalledOnceWithInstanceOfApplicationConfigurationAndConfigurationArray(
+        string $configurationClassName,
+        array $configurationArray
+    ): void {
+        \Phake::verify($this->configurationProcessor, \Phake::times(1))
+            ->processConfiguration(\Phake::capture($configurationClass), $configurationArray);
+        $this->assertInstanceOf($configurationClassName, $configurationClass);
+    }
+
+    private function assertConfigurationFactory_createConfiguration_isCalledOnceWithArray(
+        array $processedConfiguration
+    ): void {
+        \Phake::verify($this->configurationFactory, \Phake::times(1))
+            ->createConfiguration($processedConfiguration);
+    }
+
+    private function givenConfigurationFactory_createConfiguration_returnsConfiguration(): Configuration
+    {
+        $configuration = \Phake::mock(Configuration::class);
+        \Phake::when($this->configurationFactory)
+            ->createConfiguration(\Phake::anyParameters())
+            ->thenReturn($configuration);
+
+        return $configuration;
+    }
+}
