658d2e759dec1d97bed57ac2346d3cca45ebb9c3
Igor Lazarev <strider2038@yandex.ru>
2018-03-11 21:35:58 +0300

    Geo map image storage factory create service

--- src/Imaging/GeoMapImageStorageFactory.php
+++ src/Imaging/GeoMapImageStorageFactory.php
@@ -11,14 +11,74 @@
 namespace Strider2038\ImgCache\Imaging;
 
 use Strider2038\ImgCache\Configuration\ImageSource\GeoMapImageSource;
+use Strider2038\ImgCache\Core\QueryParameter;
+use Strider2038\ImgCache\Core\QueryParameterCollection;
+use Strider2038\ImgCache\Imaging\Extraction\GeoMapExtractor;
+use Strider2038\ImgCache\Imaging\Image\ImageFactoryInterface;
+use Strider2038\ImgCache\Imaging\Parsing\GeoMap\GeoMapParametersParserInterface;
+use Strider2038\ImgCache\Imaging\Storage\Accessor\GeoMapStorageAccessor;
+use Strider2038\ImgCache\Imaging\Storage\Converter\YandexMapParametersConverter;
+use Strider2038\ImgCache\Imaging\Storage\Data\YandexMapParametersFactory;
+use Strider2038\ImgCache\Imaging\Storage\Driver\ApiStorageDriver;
+use Strider2038\ImgCache\Utility\EntityValidatorInterface;
+use Strider2038\ImgCache\Utility\HttpClientFactoryInterface;
 
 /**
  * @author Igor Lazarev <strider2038@rambler.ru>
  */
 class GeoMapImageStorageFactory
 {
+    private const YANDEX_MAP_BASE_URI = 'https://static-maps.yandex.ru/1.x/';
+
+    /** @var GeoMapParametersParserInterface */
+    private $parametersParser;
+    /** @var EntityValidatorInterface */
+    private $validator;
+    /** @var ImageFactoryInterface */
+    private $imageFactory;
+    /** @var HttpClientFactoryInterface */
+    private $httpClientFactory;
+
+    public function __construct(
+        GeoMapParametersParserInterface $parametersParser,
+        EntityValidatorInterface $validator,
+        ImageFactoryInterface $imageFactory,
+        HttpClientFactoryInterface $httpClientFactory
+    ) {
+        $this->parametersParser = $parametersParser;
+        $this->validator = $validator;
+        $this->imageFactory = $imageFactory;
+        $this->httpClientFactory = $httpClientFactory;
+    }
+
     public function createImageStorageForImageSource(GeoMapImageSource $imageSource): ImageStorageInterface
     {
+        $parametersConverter = new YandexMapParametersConverter(
+            new YandexMapParametersFactory(
+                $this->validator
+            )
+        );
 
+        $httpClient = $this->httpClientFactory->createClient([
+            'base_uri' => self::YANDEX_MAP_BASE_URI
+        ]);
+
+        $storageDriver = new ApiStorageDriver(
+            $httpClient,
+            new QueryParameterCollection([
+                new QueryParameter('key', $imageSource->getApiKey())
+            ])
+        );
+
+        return new ImageStorage(
+            new GeoMapExtractor(
+                $this->parametersParser,
+                new GeoMapStorageAccessor(
+                    $parametersConverter,
+                    $storageDriver,
+                    $this->imageFactory
+                )
+            )
+        );
     }
 }
---
+++ tests/Unit/Imaging/GeoMapImageStorageFactoryTest.php
@@ -0,0 +1,96 @@
+<?php
+/*
+ * This file is part of ImgCache.
+ *
+ * (c) Igor Lazarev <strider2038@rambler.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strider2038\ImgCache\Tests\Unit\Imaging;
+
+use PHPUnit\Framework\TestCase;
+use Strider2038\ImgCache\Configuration\ImageSource\GeoMapImageSource;
+use Strider2038\ImgCache\Imaging\GeoMapImageStorageFactory;
+use Strider2038\ImgCache\Imaging\Image\ImageFactoryInterface;
+use Strider2038\ImgCache\Imaging\ImageStorage;
+use Strider2038\ImgCache\Imaging\Parsing\GeoMap\GeoMapParametersParserInterface;
+use Strider2038\ImgCache\Utility\EntityValidatorInterface;
+use Strider2038\ImgCache\Utility\HttpClientFactoryInterface;
+use Strider2038\ImgCache\Utility\HttpClientInterface;
+
+class GeoMapImageStorageFactoryTest extends TestCase
+{
+    private const CACHE_DIRECTORY = 'cache_directory';
+    private const DRIVER = 'yandex';
+    private const API_KEY = 'api_key';
+    private const YANDEX_MAP_BASE_URI = 'https://static-maps.yandex.ru/1.x/';
+
+    /** @var GeoMapParametersParserInterface */
+    private $parametersParser;
+    /** @var EntityValidatorInterface */
+    private $validator;
+    /** @var ImageFactoryInterface */
+    private $imageFactory;
+    /** @var HttpClientFactoryInterface */
+    private $httpClientFactory;
+
+    protected function setUp(): void
+    {
+        $this->parametersParser = \Phake::mock(GeoMapParametersParserInterface::class);
+        $this->validator = \Phake::mock(EntityValidatorInterface::class);
+        $this->imageFactory = \Phake::mock(ImageFactoryInterface::class);
+        $this->httpClientFactory = \Phake::mock(HttpClientFactoryInterface::class);
+    }
+
+    /** @test */
+    public function createImageStorageForImageSource_givenGeoMapImageSource_imageStorageCreatedAndReturned(): void
+    {
+        $imageStorageFactory = $this->createGeoMapImageStorageFactory();
+        $imageSource = $this->givenGeoMapImageSource();
+        $this->givenHttpClientFactory_createClient_returnsHttpClient();
+
+        $imageStorage = $imageStorageFactory->createImageStorageForImageSource($imageSource);
+
+        $this->assertInstanceOf(ImageStorage::class, $imageStorage);
+        $this->assertHttpClientFactory_createClient_isCalledOnceWithExpectedParameters();
+    }
+
+    private function createGeoMapImageStorageFactory(): GeoMapImageStorageFactory
+    {
+        return new GeoMapImageStorageFactory(
+            $this->parametersParser,
+            $this->validator,
+            $this->imageFactory,
+            $this->httpClientFactory
+        );
+    }
+
+    private function givenGeoMapImageSource(): GeoMapImageSource
+    {
+        return new GeoMapImageSource(
+            self::CACHE_DIRECTORY,
+            self::DRIVER,
+            self::API_KEY
+        );
+    }
+
+    private function givenHttpClientFactory_createClient_returnsHttpClient(): void
+    {
+        $client = \Phake::mock(HttpClientInterface::class);
+        \Phake::when($this->httpClientFactory)
+            ->createClient(\Phake::anyParameters())
+            ->thenReturn($client);
+    }
+
+    private function assertHttpClientFactory_createClient_isCalledOnceWithExpectedParameters(): void
+    {
+        $parameters = [
+            'base_uri' => self::YANDEX_MAP_BASE_URI,
+        ];
+
+        \Phake::verify($this->httpClientFactory, \Phake::times(1))
+            ->createClient($parameters);
+    }
+}
