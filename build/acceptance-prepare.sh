dfe73a585656a7601aec05058cf1a42f00365925
strider <strider2038@yandex.ru>
2018-03-18 01:09:17 +0300

    Acceptance test in CI debugging

--- build/acceptance-prepare.sh
+++ build/acceptance-prepare.sh
@@ -6,12 +6,6 @@ container_tag="strider2038/image-cache"
 echo "Preparing container for acceptance testing..."
 echo "========================================================================="
 
-echo "Preparing folders..."
-mkdir -p ./runtime/tests/acceptance/web
-mkdir -p ./runtime/tests/acceptance/storage
-chmod 0777 ./runtime/tests/acceptance/web
-chmod 0777 ./runtime/tests/acceptance/storage
-
 echo "Starting container..."
 docker run \
     --publish 127.0.0.1:1234:80 \
@@ -19,12 +13,12 @@ docker run \
     --name "$container_name" \
     --stop-signal SIGKILL \
     --env APP_CONFIGURATION_FILENAME=config/testing/acceptance-parameters.yml \
-    --volume $PWD/runtime/tests/acceptance/web:/app/web \
-    --volume $PWD/runtime/tests/acceptance/storage:/tmp/storage \
     "$container_tag"
 
 docker ps
 docker logs "$container_name"
+docker exec -it "$container_name" sh -c "supervisorctl status"
+sleep 5
 
 curl -v localhost:1234
 curl -v 0.0.0.0:1234
