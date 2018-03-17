3721431bcb47293431f408cc6d95c18cf4463415
strider <strider2038@yandex.ru>
2018-03-17 16:52:11 +0300

    Acceptance testing script in CI

---
+++ build/acceptance-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+container_name="image-cache"
+container_tag="strider2038/image-cache"
+
+echo "Preparing container for acceptance testing..."
+echo "========================================================================="
+
+echo "Preparing folders..."
+mkdir -p ./runtime/tests/acceptance/web
+mkdir -p ./runtime/tests/acceptance/storage
+
+echo "Starting container..."
+docker run \
+    --detach \
+    --name "$container_name" \
+    --stop-signal SIGKILL \
+    --env APP_CONFIGURATION_FILENAME=config/testing/acceptance-parameters.yml \
+    --volume $PWD/runtime/tests/acceptance/web:/app/web \
+    --volume $PWD/runtime/tests/acceptance/storage:/tmp/storage \
+    "$container_tag"
+
+echo "Starting acceptance testing..."
+./vendor/bin/phpunit --testsuite acceptance
+
+docker stop "$container_name"
+docker rm "$container_name"
--- build/before.sh
+++ build/before.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "{\"github-oauth\": {\"github.com\": \"$GITHUB_OAUTH\"}}" >> .docker/composer/auth.json
+echo "{\"github-oauth\": {\"github.com\": \"$GITHUB_OAUTH\"}}" >> .docker/files/var/run/composer/auth.json
